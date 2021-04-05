637b855170a8acd351e52ec5689c74a6f7db16cd
Igor Alpoim O. Puppim <igor.puppim@gmail.com>
2021-04-05 16:58:51 -0300

    Added compression task to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 const gulp = require('gulp');
 const pug  = require('gulp-pug-3');
 const sass = require('gulp-sass');
+const minify = require('gulp-minify');
+ 
+gulp.task('compress', function() {
+  gulp.src(['src/js/*.js', 'src/js/*.mjs'])
+    .pipe(minify())
+    .pipe(gulp.dest('dist'))
+});
 
 gulp.task('pug', function(){
     return gulp.src('src/pug/*.pug')
@@ -14,8 +21,15 @@ gulp.task('sass', function(){
             .pipe(gulp.dest('public/sass'));
 }); 
 
+gulp.task('sass', function(){
+    return gulp.src('src/sass/*.sass')
+            .pipe(sass())
+            .pipe(gulp.dest('public/sass'));
+}); 
+
 gulp.task('default', function(){
-    gulp.watch('src/pug/*', gulp.series('pug'));
+    gulp.watch('src/pug/*',  gulp.series('pug'));
     gulp.watch('src/sass/*', gulp.series('sass'));
+    gulp.watch('src/js/*',   gulp.series('compress'));
     return
 });
